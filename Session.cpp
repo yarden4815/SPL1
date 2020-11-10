ef0b96217e8282aa86251cd70134628bbcae9c2b
guyedv <guyedv@post.bgu.ac.il>
2020-11-10 21:14:04 +0200

    fix for BFS

--- Session.cpp
+++ Session.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include "Graph.h"
 #include "Session.h"
+#include "Tree.h"
 #include "queue"
 
 
@@ -12,18 +13,23 @@ Session::Session(const std::string &path) {}
 
 Tree * Session::BFS(int node) {
     Tree* tree = new Tree(node);
-    std::queue<int> queue;
-    std::vector<vector<int>> edges = g.getEdges();
-    std::vector<bool> visit(g.getSize(),false);
-    queue.push(node);
-    visit[node] = true;
-    int temp = 0;
-    while(!queue.empty()){
-        for(int i = 0; i < g.getSize(); i++){
-            if(edges[node][i] == 1 & !visit[i]) {
+    std::queue<*Tree> queue;
+    std::vector<int> neighbours = g.getNeighbours(node);
+    std::vector<bool> visited(g.getSize(),false);
+    queue.push(tree);
+    visited[node] = true;
 
-                queue.push(edges[node][i]);
-                visit[i] = true;
+    while(!queue.empty()){
+        Tree* temp = queue.pop();
+        for(int i = 0; i < neighbours.size(); i++){
+            if(neighbours[i] == 1 && !visit[i]) {
+                Tree* child = new Tree(i);
+                temp->addChild(child);
+                temp = child;
+                queue.push(child);
+                visited[i] = true;
+                neighbours = g.getNeighbours(i);
+
         }
         }
 
